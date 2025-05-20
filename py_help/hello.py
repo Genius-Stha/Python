469dd582db9b59f8664e5449f09da07833f0f72f
Genius-Stha <geniusstha1@gmail.com>
2025-05-21 02:41:58 +0545

    updated

--- py_help/hello.py
+++
@@ -1,5 +0,0 @@
-mess="hello             "
-mess2="           world"
-mess3 = 10110 
-print(mess.rstrip().title()+ " " + mess2.lstrip().upper()+str(mess3))
-#python cannot concatenate the string with integer so changeing to stirng
